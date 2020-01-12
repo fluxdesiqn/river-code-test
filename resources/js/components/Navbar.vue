510eb948bbec3619f05ee060840931f4a0a9987b
Connor Shaw <connor6shaw@gmail.com>
2020-01-13 05:05:45 +1300

    adding in vue and navbar block

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand"><img src="https://weareriver.nz/assets/images/company/logo-light.svg"></a>
+        </div>
+    </nav>
+</template>
--- resources/views/template/master.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @extends('includes.header')
-    </head>
-    <body>
-        @yield('content')
-    </body>
-</html>
